f696948b645f06b6ac3a4b7cbc2fa1987afb9d30
AlexFromSiberia <id764g@gmail.com>
2022-08-11 13:58:12 +0700

    Finished

--- study_pr/study_pr/settings.py
+++ study_pr/study_pr/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-$5yp+q%e@0y95mnz09@w-+x9a7y-wrl%*4wrvvf9cf*dfci-g*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
